26c105961f43db72f34e9592269ec4747012378f
getinthere <getinthere@naver.com>
2023-01-19 14:02:25 +0900

    쿠키로 사용자 유저네임 기억하기

--- src/main/java/shop/mtcoding/buyer/controller/UserController.java
+++ src/main/java/shop/mtcoding/buyer/controller/UserController.java
@@ -1,9 +1,13 @@
 package shop.mtcoding.buyer.controller;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -19,16 +23,43 @@ public class UserController {
     private HttpSession session;
 
     @GetMapping("/loginForm")
-    public String loginForm() {
+    public String loginForm(HttpServletRequest request) {
+        // JSESSIONID=6A5470C7B10910305F1AC87C6DAFD0AF
+        String username = "";
+
+        // ${cookie.remember.value}
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("remember")) {
+                username = cookie.getValue();
+            }
+        }
+        request.setAttribute("remember", username);
         return "user/loginForm";
     }
 
     @PostMapping("/login")
-    public String login(String username, String password) {
+    public String login(String username, String password, String remember, HttpServletResponse response) {
         User user = userRepository.findByUsernameAndPassword(username, password);
         if (user == null) {
             return "redirect:/loginForm";
         } else {
+            // 요청헤더 : Cookie
+            // 응답헤더 : Set-Cookie
+            if (remember == null) {
+                remember = "";
+            }
+
+            if (remember.equals("on")) {
+                // Set-Cookie
+                Cookie cookie = new Cookie("remember", username);
+                response.addCookie(cookie);
+            } else {
+                Cookie cookie = new Cookie("remember", "");
+                cookie.setMaxAge(0);
+                response.addCookie(cookie);
+            }
+
             session.setAttribute("principal", user);
             return "redirect:/";
         }
