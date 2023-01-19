6527ca5a1ae56b81863dbc9598af85655d1be265
getinthere <getinthere@naver.com>
2023-01-19 16:14:27 +0900

    상품목록 기능 완료

--- src/main/java/shop/mtcoding/buyer/BuyerApplication.java
+++ src/main/java/shop/mtcoding/buyer/BuyerApplication.java
@@ -3,10 +3,13 @@ package shop.mtcoding.buyer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import shop.mtcoding.buyer.controller.ProductController;
+
 @SpringBootApplication
 public class BuyerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BuyerApplication.class, args);
 	}
 
--- src/main/java/shop/mtcoding/buyer/controller/ProductController.java
+++ src/main/java/shop/mtcoding/buyer/controller/ProductController.java
@@ -1,13 +1,28 @@
 package shop.mtcoding.buyer.controller;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import shop.mtcoding.buyer.model.Product;
+import shop.mtcoding.buyer.model.ProductRepository;
 
 @Controller
 public class ProductController {
 
+    @Autowired // DI
+    private ProductRepository productRepository;
+
     @GetMapping({ "/", "/product" })
-    public String home() {
+    public String home(Model model) {
+        List<Product> productList = productRepository.findAll();
+        model.addAttribute("productList", productList);
         return "product/list";
     }
 }
