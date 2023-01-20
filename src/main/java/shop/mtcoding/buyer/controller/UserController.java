e810b1d83d8a00e4d30a2efa86e579ecb0070a8d
getinthere <getinthere@naver.com>
2023-01-20 16:36:01 +0900

    구매 목록 보기 완료

--- src/main/java/shop/mtcoding/buyer/controller/UserController.java
+++ src/main/java/shop/mtcoding/buyer/controller/UserController.java
@@ -49,26 +49,24 @@ public class UserController {
         User user = userRepository.findByUsernameAndPassword(username, password);
         if (user == null) {
             return "redirect:/loginForm";
-        } else {
-            // 요청헤더 : Cookie
-            // 응답헤더 : Set-Cookie
-            if (remember == null) {
-                remember = "";
-            }
-
-            if (remember.equals("on")) {
-                // Set-Cookie
-                Cookie cookie = new Cookie("remember", username);
-                response.addCookie(cookie);
-            } else {
-                Cookie cookie = new Cookie("remember", "");
-                cookie.setMaxAge(0);
-                response.addCookie(cookie);
-            }
-
-            session.setAttribute("principal", user);
-            return "redirect:/";
         }
+        // 요청헤더 : Cookie
+        // 응답헤더 : Set-Cookie
+        if (remember == null) {
+            remember = "";
+        }
+
+        if (remember.equals("on")) {
+            Cookie cookie = new Cookie("remember", username);
+            response.addCookie(cookie);
+        } else {
+            Cookie cookie = new Cookie("remember", "");
+            cookie.setMaxAge(0);
+            response.addCookie(cookie);
+        }
+
+        session.setAttribute("principal", user);
+        return "redirect:/";
 
     }
 
